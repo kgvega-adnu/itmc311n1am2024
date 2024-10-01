8299180cc9d22ccac126c68993d1a3aa2ead17f7
ILGeneral <igeneral@gbox.adnu.edu.ph>
2024-10-01 16:20:56 +0800

    chore: add project TeamADLearn_ADLearn directory

---
+++ ADLearn/firebase.js
@@ -0,0 +1,12 @@
+
+const register=document.getElementById('register');
+
+
+    
+const submitbtn = document.getElementById('sub-btn');
+
+submitbtn.addEventListener('click', (e) => {
+    const email = document.getElementById('email').value;
+    const pass = document.getElementById('pass').value;
+
+})
